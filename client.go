c729b922cf01f242a2125eeaa407643749121133
Dmitry Paramonov <gamiysikdima@gmail.com>
2020-10-22 05:11:19 +0300

    cors checking disabled

--- client.go
+++ client.go
@@ -138,6 +138,8 @@ func generateClientId() string {
 
 // serveWs handles websocket requests from the peer.
 func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	//TODO: CORS checking
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
